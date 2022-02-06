d79066400c74cc251051d052a102e9321829bc2f
nikosChalk <chalkiadakisn@gmail.com>
2022-02-06 21:04:44 +0200

    better battery stats UI

---
+++ app/src/main/java/nl/vu/cs/s2group/batterybomber/HorizontalSeparator.kt
@@ -0,0 +1,11 @@
+package nl.vu.cs.s2group.batterybomber
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+
+class HorizontalSeparator @JvmOverloads constructor(
+    context: Context,
+    attributeSet: AttributeSet? = null,
+    defStyleAttr: Int = R.style.horizontal_separator,
+): View(context, attributeSet, defStyleAttr)
