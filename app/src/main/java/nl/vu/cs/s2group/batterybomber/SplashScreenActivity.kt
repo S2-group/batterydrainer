58eb5c99d3036749778551725305734de7565e5c
nikosChalk <chalkiadakisn@gmail.com>
2022-02-13 23:52:14 +0200

    splash screen for Android 12

--- app/src/main/java/nl/vu/cs/s2group/batterybomber/SplashScreenActivity.kt
+++ app/src/main/java/nl/vu/cs/s2group/batterybomber/SplashScreenActivity.kt
@@ -2,13 +2,20 @@ package nl.vu.cs.s2group.batterybomber
 
 import android.annotation.SuppressLint
 import android.content.Intent
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 
 @SuppressLint("CustomSplashScreen")
 class SplashScreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+        if(Build.VERSION.SDK_INT < Build.VERSION_CODES.S) {
+            super.onCreate(savedInstanceState)
+        } else {
+            installSplashScreen()
+            super.onCreate(savedInstanceState)
+        }
         Thread.sleep(500) //artificial delay before we switch from the splash screen
         startActivity(Intent(this@SplashScreenActivity, MainActivity::class.java))
         finish()
